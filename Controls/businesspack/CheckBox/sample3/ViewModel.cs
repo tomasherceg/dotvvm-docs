87af546c139f3eaf93a6c8a336a818e6686759b9
Dušan Janošík <dusan@djanosik.cz>
2017-08-24 15:14:41 +0200

    Adds missing CheckBox samples.

---
+++ Controls/businesspack/CheckBox/sample3/ViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace DotvvmWeb.Views.Docs.Controls.businesspack.CheckBox.sample3
+{
+    public class ViewModel
+    {
+        public bool Value { get; set; }
+
+        public int NumberOfChanges { get; set; } = 0;
+
+        public void OnChanged()
+        {
+            NumberOfChanges++;
+        }
+    }
+}
