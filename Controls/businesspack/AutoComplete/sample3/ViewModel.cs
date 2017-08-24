edf546277820452a475f208de256d91262a8464a
Dušan Janošík <dusan@djanosik.cz>
2017-08-24 15:08:45 +0200

    Improves AutoComplete samples.

---
+++ Controls/businesspack/AutoComplete/sample3/ViewModel.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+
+namespace DotvvmWeb.Views.Docs.Controls.businesspack.AutoComplete.sample3
+{
+    public class ViewModel
+    {
+        public string City { get; set; }
+
+        public int CityChangeCount { get; set; }
+
+        public IEnumerable<string> Suggestions { get; set; } = new List<string> {
+            "Atlantic City",
+            "Boston",
+            "Chicago",
+            "Dallas",
+            "New York",
+            "Washington D.C.",
+            "Miami",
+            "San Francisco"
+        };
+
+        public void CityChanged() => CityChangeCount++;
+    }
+}
