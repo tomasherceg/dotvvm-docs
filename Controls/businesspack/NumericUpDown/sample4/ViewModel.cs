68b6d04386c12fa471dd5dc2372d19ca72a7b428
Dušan Janošík <dusan@djanosik.cz>
2017-08-24 16:44:03 +0200

    Improves NumericUpDown samples.

---
+++ Controls/businesspack/NumericUpDown/sample4/ViewModel.cs
@@ -0,0 +1,9 @@
+using DotVVM.BusinessPack.Controls;
+
+namespace DotvvmWeb.Views.Docs.Controls.businesspack.NumericUpDown.sample4
+{
+    public class ViewModel
+    {
+        public double Value { get; set; } = 1.5;
+    }
+}
