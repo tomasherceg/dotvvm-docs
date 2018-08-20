088eda086392a5f26f1bbff25420d1754a7ea367
Tomáš Herceg <tomas.herceg@riganti.cz>
2018-08-20 20:49:39 +0200

    BS4 - FormGroup

---
+++ Controls/bootstrap4/FormGroup/sample2/ViewModel.cs
@@ -0,0 +1,22 @@
+﻿using DotVVM.Framework.ViewModel;
+
+namespace DotvvmWeb.Views.Docs.Controls.bootstrap.FormGroup.sample2
+{
+    public class ViewModel : DotvvmViewModelBase
+    {
+        public bool Red { get; set; } = false;
+        public bool Blue { get; set; } = true;
+        public string Text { get; set; }
+        public void DoSomething()
+        {
+            if(Red && Blue)
+            {
+                Text = "You select two colors";
+            }
+            else
+            {
+                Text = "You select only one color";
+            }
+        }
+    }
+}
---
+++ Controls/bootstrap4/FormGroup/sample2/sample.md
@@ -0,0 +1,3 @@
+## Sample 2: FormGroup LabelTemplate
+
+If you need custom content in the label, just use the `LabelTemplate` property instead of `LabelText`.
