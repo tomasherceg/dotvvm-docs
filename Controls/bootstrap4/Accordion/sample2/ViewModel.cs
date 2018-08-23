1edb0b109a3a2dd6b78e0a3dfcbface958bf3e37
Tomáš Herceg <tomas.herceg@riganti.cz>
2018-08-23 20:26:25 +0200

    BS4 - Accordion

---
+++ Controls/bootstrap4/Accordion/sample2/ViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DotvvmWeb.Views.Docs.Controls.bootstrap.Accordion.sample2
+{
+    public class ViewModel
+    {
+        public int Index { get; set; } = 1;
+
+        public PanelData[] Panels { get; set; } =
+        {
+            new PanelData("Text 1"),
+            new PanelData("Text 2"),
+            new PanelData("Text 3")
+        };
+    }
+
+    public class PanelData
+    {
+        public string Text { get; set; }
+
+        public PanelData()
+        {
+        }
+
+        public PanelData(string text)
+        {
+            Text = text;
+        }
+    }
+}
---
+++ Controls/bootstrap4/AccordionItem/control.md
@@ -0,0 +1,7 @@
+Represents an item in [Accordion](/docs/controls/bootstrap4/Accordion/{branch}) control.
+
+This control inherits from [Card](/docs/controls/bootstrap4/Card/{branch}). 
+
+It has an additional property `IsExpanded` which determines whether the card content is expanded or not.
+
+<https://getbootstrap.com/docs/4.1/components/collapse/#accordion-example>
