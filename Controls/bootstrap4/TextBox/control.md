79b47c2cda481f6e6374ffbc380f8e7e8be5a08a
Michal Tichý <michal.tichy@riganti.cz>
2018-10-08 10:44:59 +0200

    Added missing controls to bs4 doc

---
+++ Controls/bootstrap4/TextBox/control.md
@@ -0,0 +1,3 @@
+Extends the builtin [TextBox](/docs/controls/builtin/TextBox) control to allow sizing.
+
+<https://getbootstrap.com/docs/4.1/components/forms/#sizing>
---
+++ Controls/bootstrap4/TextBox/sample1/ViewModel.cs
@@ -0,0 +1,9 @@
+﻿using DotVVM.Framework.ViewModel;
+
+namespace DotvvmWeb.Views.Docs.Controls.bootstrap4.TextxBox.sample1
+{
+    public class ViewModel : DotvvmViewModelBase
+    {
+        public string Text { get; set; }
+    }
+}
