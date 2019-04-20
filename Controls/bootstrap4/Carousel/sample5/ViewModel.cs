edbc3f419d760d7a140dcd6983b0b9f9a432951d
Tomáš Herceg <herceg@vbnet.cz>
2019-04-20 20:24:40 +0200

    Bootstrap 4 samples - images fixed

--- Controls/bootstrap4/Carousel/sample5/ViewModel.cs
+++ Controls/bootstrap4/Carousel/sample5/ViewModel.cs
@@ -1,35 +1,27 @@
-﻿using DotVVM.Framework.ViewModel;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace DotvvmWeb.Views.Docs.Controls.bootstrap.Carousel.sample5
+﻿public class ViewModel : DotvvmViewModelBase
 {
-    public class ViewModel : DotvvmViewModelBase
+    public ImageData[] Images { get; set; } = new[]
     {
-        public ImageData[] Images { get; set; } = new[]
-        {
-            new ImageData("/bootstrap/Images/LA.jpg", "Los Angeles"),
-            new ImageData("/bootstrap/Images/NY.jpg", "New York", true),
-            new ImageData("/bootstrap/Images/Miami.jpg", "Miami")
-        };
+        new ImageData("../../Images/LA.jpg", "Los Angeles"),
+        new ImageData("../../Images/NY.jpg", "New York", true),
+        new ImageData("../../Images/Miami.jpg", "Miami")
+    };
+}
+
+public class ImageData
+{
+    public ImageData()
+    {
     }
 
-    public class ImageData
+    public ImageData(string url, string caption, bool active = false)
     {
-        public ImageData()
-        {
-        }
-
-        public ImageData(string url, string caption, bool active = false)
-        {
-            IsActive = active;
-            Url = url;
-            Caption = caption;
-        }
-
-        public string Caption { get; set; }
-        public bool IsActive { get; set; }
-        public string Url { get; set; }
+        IsActive = active;
+        Url = url;
+        Caption = caption;
     }
+
+    public string Caption { get; set; }
+    public bool IsActive { get; set; }
+    public string Url { get; set; }
 }
