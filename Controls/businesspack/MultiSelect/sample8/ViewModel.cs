6a91648e76bbac19c286a13cb215dc3bcdd6f19b
Maroš Janota <maros.janota@riganti.cz>
2019-05-07 13:38:32 +0200

    City Names edited

--- Controls/businesspack/MultiSelect/sample8/ViewModel.cs
+++ Controls/businesspack/MultiSelect/sample8/ViewModel.cs
@@ -6,9 +6,9 @@ namespace DotvvmWeb.Views.Docs.Controls.businesspack.MultiSelect.sample8
     public class ViewModel : DotvvmViewModelBase
     {
         public List<City> Cities { get; set; } = new List<City> {
-            new City { Id = 1, Name = "Washington", Country = "Utah" },
-            new City { Id = 2, Name = "Washington", Country = "Kansas" },
-            new City { Id = 3, Name = "Washington", Country = "Georgia" }
+            new City { Id = 1, Name = "Salt Lake City", Country = "Utah" },
+            new City { Id = 2, Name = "Wichita", Country = "Kansas" },
+            new City { Id = 3, Name = "Atlanta", Country = "Georgia" }
         };
 
         public List<City> SelectedCities { get; set; } = new List<City>();
