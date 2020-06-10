11226e5b8be179fdf1b562681ead3021074843d1
Michal Mrnustik <michal.mrnustik@outlook.com>
2020-06-10 13:53:58 +0200

    Add more information to restriction samples

--- Controls/businesspack/TimePicker/sample3/ViewModel.cs
+++ Controls/businesspack/TimePicker/sample3/ViewModel.cs
@@ -11,12 +11,14 @@ namespace DotvvmWeb.Views.Docs.Controls.businesspack.TimePicker.sample3
 
         public List<TimeRestriction> Restrictions { get; set; } = new List<TimeRestriction>()
         {
+            //This will prevent selection of any time between 11:45:00 to 13:14:59
             new TimeRangeRestriction() {
                 StartTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day,
                     11, 45, 0),
                 EndTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day,
                     13, 15, 0)
             },
+            //This will prevent selection of any time between 16:30:00 to 17:29:59
             new TimeRangeRestriction() {
                 StartTime = new DateTime(DateTime.Today.Year, DateTime.Today.Month, DateTime.Today.Day,
                     16, 30, 0),
--- Controls/businesspack/TimePicker/sample3/sample.md
+++ Controls/businesspack/TimePicker/sample3/sample.md
@@ -2,4 +2,4 @@
 
 If you require more granular control over what dates can be selected, you can use the `Restrictions` property.
 
-- **TimeRangeRestriction** - Allows to disable selection of a specific time.
+- **TimeRangeRestriction** - Allows to disable selection of a specific time range. You just need to set the `StartTime` and `EndTime` properties, where the `StartTime` represents inclusive start of the restriction and `EndTime` represents end of the restriction. Date part of the `DateTime` values is ignored so any date can be provided.
