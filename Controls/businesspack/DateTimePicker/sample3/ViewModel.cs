0a811c99a0a654076fe68d464eb6ab7056dee5eb
Michal Mrnustik <michal.mrnustik@outlook.com>
2020-06-10 13:46:36 +0200

    Add more information about restrictions to the DateTimePicker sample

--- Controls/businesspack/DateTimePicker/sample3/ViewModel.cs
+++ Controls/businesspack/DateTimePicker/sample3/ViewModel.cs
@@ -11,9 +11,15 @@ namespace DotvvmWeb.Views.Docs.Controls.businesspack.DateTimePicker.sample3
 
         public List<DateTimeRestriction> Restrictions { get; set; } = new List<DateTimeRestriction>()
         {
+            //Theese will prevent saturdays and sundays from being selected
             new DayOfWeekRestriction() { DayOfWeek = DayOfWeek.Saturday },
             new DayOfWeekRestriction() { DayOfWeek = DayOfWeek.Sunday },
-            new DateRangeRestriction() { StartDate = DateTime.MinValue, EndDate = DateTime.Now }
+            //This will prevent selection of all dates from the 1st of June to the 6th of June of current year
+            new DateRangeRestriction() 
+            { 
+                StartDate = new DateTime(DateTime.Today.Year, 6, 1), 
+                EndDate = new DateTime(DateTime.Today.Year, 6, 7)
+            }
         };
     }
 }
